17505e319f425ba5f7dbf5514fa42a65719881d8
Chris Pruett <chris@jam.ai>
2023-03-07 22:17:31 -0800

    Repro

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-native/no-inline-styles */
-import {useCallback} from 'react';
 import {Pressable, Text, View} from 'react-native';
 import {
   NavigationContainer,
@@ -12,13 +11,8 @@ import {
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
 
-type EmbeddedHomeStackParamList = {
-  Home: undefined;
-  DeepEmbedded: undefined;
-};
-
 type TabNavigatorParamList = {
-  EmbeddedHomeStack: NavigatorScreenParams<EmbeddedHomeStackParamList>;
+  Home: undefined;
   AnotherTab: undefined;
 };
 
@@ -30,14 +24,10 @@ type RootStackParamList = {
 
 const Tab = createBottomTabNavigator();
 const RootStack = createNativeStackNavigator<RootStackParamList>();
-const HomeStack = createNativeStackNavigator<EmbeddedHomeStackParamList>();
 
 function HomeScreen() {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const openModal = useCallback(() => {
-    navigation.push('ModalOne');
-  }, [navigation]);
 
   return (
     <View
@@ -46,73 +36,21 @@ function HomeScreen() {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Pressable onPress={openModal}>
-        <Text>Open modal</Text>
+      <Pressable onPress={() => navigation.push('ModalOne')}>
+        <Text style={{color: 'purple'}}>Open first modal</Text>
       </Pressable>
     </View>
   );
 }
 
 function AnotherTabScreen() {
-  return (
-    <View
-      style={{
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Just another tab</Text>
-    </View>
-  );
-}
-
-function DeepEmbeddedScreen() {
-  return (
-    <View
-      style={{
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Deep embedded screen</Text>
-    </View>
-  );
-}
-
-function EmbeddedHomeStack() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Group
-        screenOptions={{
-          title: '',
-          headerBackTitleVisible: false,
-          headerStyle: {backgroundColor: '#fff'},
-          headerShadowVisible: false,
-          headerTintColor: '#121212',
-        }}>
-        <HomeStack.Screen
-          name="Home"
-          options={{headerShown: false}}
-          component={HomeScreen}
-        />
-        <HomeStack.Screen
-          name="DeepEmbedded"
-          options={{headerShown: true}}
-          component={DeepEmbeddedScreen}
-        />
-      </HomeStack.Group>
-    </HomeStack.Navigator>
-  );
+  return <View />;
 }
 
 function TabNavigator() {
   return (
-    <Tab.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName="EmbeddedHomeStack"
-      backBehavior="history">
-      {/* tab bar isn't customized (ruled out by removing custom tab bar in our app) */}
-      <Tab.Screen name="EmbeddedHomeStack" component={EmbeddedHomeStack} />
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="AnotherTab" component={AnotherTabScreen} />
     </Tab.Navigator>
   );
@@ -121,39 +59,16 @@ function TabNavigator() {
 function ModalOne() {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const gotoDeepEmbedded = useCallback(
-    () =>
-      navigation.replace('TabNavigator', {
-        screen: 'EmbeddedHomeStack',
-        params: {
-          screen: 'DeepEmbedded',
-          initial: false,
-        },
-      }),
-    [navigation],
-  );
-
-  const gotoModalTwo = useCallback(
-    () => navigation.replace('ModalTwo'),
-    [navigation],
-  );
 
   return (
     <View
       style={{
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingVertical: 25,
-        flex: 1,
-        backgroundColor: 'purple',
-        gap: 20,
       }}>
-      <Text>Modal</Text>
-      <Pressable onPress={gotoDeepEmbedded}>
-        <Text>Goto DeepEmbedded</Text>
-      </Pressable>
-      <Pressable onPress={gotoModalTwo}>
-        <Text>Goto Modal Two</Text>
+      <Pressable onPress={() => navigation.replace('ModalTwo')}>
+        <Text style={{color: 'purple'}}>Goto second modal</Text>
       </Pressable>
     </View>
   );
@@ -162,25 +77,18 @@ function ModalOne() {
 function ModalTwo() {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const pop = useCallback(
-    () => navigation.pop(),
 
-    [navigation],
-  );
+  console.info('Rendering ModalTwo');
 
   return (
     <View
       style={{
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingVertical: 25,
-        flex: 1,
-        backgroundColor: 'purple',
-        gap: 20,
       }}>
-      <Text>Another Modal</Text>
-      <Pressable onPress={pop}>
-        <Text>Close</Text>
+      <Pressable onPress={() => navigation.pop()}>
+        <Text style={{color: 'purple'}}>Close</Text>
       </Pressable>
     </View>
   );
@@ -189,26 +97,15 @@ function ModalTwo() {
 export default function App() {
   return (
     <NavigationContainer>
-      {/* Diff: No linking config, no datadog (ruled out by deleting both in our app)*/}
-      <RootStack.Navigator screenOptions={{headerShown: false}}>
+      <RootStack.Navigator>
+        <RootStack.Screen name="TabNavigator" component={TabNavigator} />
         <RootStack.Screen
-          name="TabNavigator"
-          component={TabNavigator}
-          options={{headerBackTitleVisible: false}}
-        />
-        <RootStack.Screen
-          options={{
-            presentation: 'modal',
-            contentStyle: {backgroundColor: 'transparent'},
-          }}
+          options={{presentation: 'modal'}}
           name="ModalOne"
           component={ModalOne}
         />
         <RootStack.Screen
-          options={{
-            presentation: 'modal',
-            contentStyle: {backgroundColor: 'transparent'},
-          }}
+          options={{presentation: 'modal'}}
           name="ModalTwo"
           component={ModalTwo}
         />
---
+++ README.md
@@ -0,0 +1,24 @@
+
+This repo demos a React Native Navigation bug when React Native new architecture is enabled.
+
+## Building
+
+1. Install dependencies
+  ```bash
+  yarn
+  cd ios
+  pod install
+  ```
+2. Start metro
+  ```bash
+  cd ..
+  yarn start
+  ```
+3. Build and run iOS app: type "i" in metro terminal
+
+## Bug reproduction
+
+1. Launch iOS app
+2. Press "Open modal"
+3. Press "Navigate to another tab"
+4. See crash
